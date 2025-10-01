f39b66a4306cc2575a6652bc7bfbd3ddbedcbc85
TheBlueBaby <theblubaby@gmail.com>
2025-10-01 16:12:25 -0400

    Updated Texture2DStorage to include blocks sprite sheet, and added the blocks / enemies sprite sheet

--- sprint0/Sprites/Texture2DStorage.cs
+++ sprint0/Sprites/Texture2DStorage.cs
@@ -3,21 +3,24 @@ using Microsoft.Xna.Framework.Graphics;
 using sprint0.Interfaces;
 using sprint0.Classes;
 using System;
+using Microsoft.Xna.Framework.Content;
 
 public static class Texture2DStorage
 {
 	public static Texture2D linkSpriteSheet;
+	public static Texture2D blockSpriteSheet;
 	public static Texture2D bossSpriteSheet;
 	public static Texture2D enemiesSpriteSheet;
 	public static Texture2D itemSpriteSheet;
 	public static Texture2D miscSpriteSheet;
 	public static SpriteFont font;
 
-	public static LoadAllTextures(ContentManager content)
+	public static void LoadAllTextures(ContentManager Content)
 	{
+		blockSpriteSheet = Content.Load<Texture2D>("legendofzelda_blocks_sheet");
 		linkSpriteSheet = Content.Load<Texture2D>("legendofzelda_link_sheet");
 		bossSpriteSheet = Content.Load<Texture2D>("legendofzelda_bosses_sheet");
-		enemiesSpriteSheet = Content.Load<Texture2D>("legendofzelda_enemies_sheet");
+		enemiesSpriteSheet = Content.Load<Texture2D>("legendofzelda_enemies_sheet_final");
 		itemSpriteSheet = Content.Load<Texture2D>("legendofzelda_items_sheet");
 		miscSpriteSheet = Content.Load<Texture2D>("legendofzelda_misccharacters_sheet");
 		font = Content.Load<SpriteFont>("font");
@@ -26,6 +29,11 @@ public static class Texture2DStorage
 	public static Texture2D GetLinkSpriteSheet()
 	{
 		return linkSpriteSheet;
+	}
+
+	public static Texture2D GetBlockSpriteSheet()
+	{
+		return blockSpriteSheet;
 	}
 	public static Texture2D GetBossSpriteSheet()
 	{
