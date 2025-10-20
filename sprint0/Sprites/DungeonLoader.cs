b6089eee19cfd18f94ab36d6d9f52c0b80dc595c
TheBlueBaby <theblubaby@gmail.com>
2025-10-19 22:52:59 -0400

    Began work on Dungeons. Modeled DungeonCarousel after its related classes. Will continue working on DungeonLoader to implement loading via CSV files.

---
+++ sprint0/Sprites/DungeonLoader.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0.Sprites
+{
+    public class DungeonLoader : ISprite
+    {
+        private BlockFactory blocks;
+
+        public DungeonLoader(BlockFactory blocks)
+        {
+            this.blocks = blocks;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch sprite, Vector2 pos)
+        {
+
+        }
+    }
+}
