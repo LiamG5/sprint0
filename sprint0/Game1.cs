e28801b66b274d660ecb9f16a6386c93c36e935b
Krush Patel <krushp04@gmail.com>
2025-10-27 23:19:55 -0400

    Running well, fixed all bugs.

--- sprint0/Game1.cs
+++ sprint0/Game1.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using sprint0.Classes;
+using sprint0.Commands;
 using sprint0.Interfaces;
 using sprint0.PlayerStates;
 using sprint0.Sprites;
@@ -12,7 +13,6 @@ using static sprint0.Sprites.BlockFactory;
 using static sprint0.Sprites.DungeonCarousel;
 using static sprint0.Sprites.EnemySpriteFactory;
 using sprint0.Collisions;
-
 
 namespace sprint0;
 
@@ -47,6 +47,19 @@ public class Game1 : Game
     private KeyboardState previousKeyboardState;
     private CollisionUpdater collisionUpdater;
 
+    // Minimap click area
+    private Rectangle mapRect = new Rectangle(32, 32, 6 * 24, 3 * 24);
+    private const int MapRows = 3;
+    private const int MapCols = 6;
+
+    // Map click → room jump
+    private Dictionary<int, ICommand> mapCellCommands;
+    private MouseController mouse;
+
+    private Texture2D _minimapOverlay;
+    private static readonly Color _minimapColor = new Color(255, 0, 0, 96);
+    private static readonly Color _minimapTextColor = Color.Yellow;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -63,40 +76,93 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+        try
+        {
             sprint0.Sprites.Texture2DStorage.LoadAllTextures(Content);
-            
-            link = new Link(_spriteBatch);
-            
-            blocks = BlockFactory.Instance;
-            enemies = EnemySpriteFactory.Instance;
-            items = ItemFactory.Instance;
-            
-            blockCarousel = new BlockCarousel(blocks, _spriteBatch);
-            enemyCarousel = new EnemyCarousel(enemies, _spriteBatch);
-            itemCarousel = new ItemCarousel(items, _spriteBatch);
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine("[LoadAllTextures] " + ex);
+        }
 
-            string dungeonPath = Path.Combine(Content.RootDirectory, "dungeon.csv");
+        _minimapOverlay = new Texture2D(GraphicsDevice, 1, 1);
+        _minimapOverlay.SetData(new[] { Color.White });
 
-            dungeon = new DungeonLoader(blocks, File.ReadAllText(dungeonPath));
-            dungeon.LoadRectangles();
+        link = new Link(_spriteBatch);
 
-            tile = blockCarousel.GetCurrentBlock();
-            enemy = enemyCarousel.GetCurrentEnemy();
-            item = itemCarousel.GetCurrentItem();
-            
+        blocks = BlockFactory.Instance;
+        enemies = EnemySpriteFactory.Instance;
+        items = ItemFactory.Instance;
+
+        blockCarousel = new BlockCarousel(blocks, _spriteBatch);
+        enemyCarousel = new EnemyCarousel(enemies, _spriteBatch);
+        itemCarousel = new ItemCarousel(items, _spriteBatch);
+
+        string dungeonPath = Path.Combine(Content.RootDirectory, "dungeon.csv");
+
+        dungeon = new DungeonLoader(blocks, File.ReadAllText(dungeonPath));
+        dungeon.LoadRectangles();
+
+        tile = blockCarousel.GetCurrentBlock();
+        enemy = enemyCarousel.GetCurrentEnemy();
+        item = itemCarousel.GetCurrentItem();
+
+        try
+        {
             Texture2D enemySheet = sprint0.Sprites.Texture2DStorage.GetEnemiesSpriteSheet();
-            var testEnemy = new EnemyKeese(enemySheet, new Vector2(400, 100));
+            var testEnemy = new EnemyGel(enemySheet, new Vector2(400, 100));
             dungeon.AddEnemy(testEnemy);
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine("[EnemySheet Load] " + ex);
+        }
 
-            controllers = new List<IController>();
-            keyboard = new KeyboardController(this, null);
-            controllers.Add(keyboard);
+        try
+        {
+            // We'll reuse this font for the minimap labels.
+            font = Content.Load<SpriteFont>("Font/font");
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine("[Font Load] " + ex);
+        }
 
-            collisionUpdater = new CollisionUpdater(dungeon, link);
-            collisionUpdater.getList();  
-            
-            previousKeyboardState = Keyboard.GetState();
+        controllers = new List<IController>();
+        keyboard = new KeyboardController(this, null);
+        controllers.Add(keyboard);
 
+        mapCellCommands = new Dictionary<int, ICommand>
+        {
+            {  0, new GoToRoom1Command(this)  },
+            {  1, new GoToRoom2Command(this)  },
+            {  2, new GoToRoom3Command(this)  },
+            {  3, new GoToRoom4Command(this)  },
+            {  4, new GoToRoom5Command(this)  },
+            {  5, new GoToRoom6Command(this)  },
+            {  6, new GoToRoom7Command(this)  },
+            {  7, new GoToRoom8Command(this)  },
+            {  8, new GoToRoom9Command(this)  },
+            {  9, new GoToRoom10Command(this) },
+            { 10, new GoToRoom11Command(this) },
+            { 11, new GoToRoom12Command(this) },
+            { 12, new GoToRoom13Command(this) },
+            { 13, new GoToRoom14Command(this) },
+            { 14, new GoToRoom15Command(this) },
+            { 15, new GoToRoom16Command(this) },
+            { 16, new GoToRoom17Command(this) },
+        };
+
+        // Mouse controller for minimap clicks
+        mouse = new MouseController(mapRect, MapRows, MapCols, mapCellCommands);
+        controllers.Add(mouse);
+
+        collisionUpdater = new CollisionUpdater(dungeon, link);
+        collisionUpdater.getList();
+
+        previousKeyboardState = Keyboard.GetState();
+
+        System.Console.WriteLine("[LoadContent] completed");
     }
 
     protected override void Update(GameTime gameTime)
@@ -124,15 +190,14 @@ public class Game1 : Game
         base.Update(gameTime);
     }
 
-
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-        
+
         _spriteBatch.Begin();
 
         dungeon.Draw(_spriteBatch, GraphicsDevice);
-        
+
         foreach (var e in dungeon.GetEnemies())
         {
             if (!e.IsDead())
@@ -140,23 +205,62 @@ public class Game1 : Game
                 e.Draw(_spriteBatch, e.GetPosition());
             }
         }
-        
-        // enemy.Draw(_spriteBatch, new Vector2(400, 100));
+
+        enemy.Draw(_spriteBatch, new Vector2(400, 100));
         item.Draw(_spriteBatch, new Vector2(200, 100));
         link.Draw(_spriteBatch);
+
+        if (_minimapOverlay != null)
+            _spriteBatch.Draw(_minimapOverlay, mapRect, _minimapColor);
+
+        DrawMinimapNumbers(_spriteBatch);
+
         _spriteBatch.End();
         base.Draw(gameTime);
     }
 
+    public void GoToRoom1()  => LoadRoom(1);
+    public void GoToRoom2()  => LoadRoom(2);
+    public void GoToRoom3()  => LoadRoom(3);
+    public void GoToRoom4()  => LoadRoom(4);
+    public void GoToRoom5()  => LoadRoom(5);
+    public void GoToRoom6()  => LoadRoom(6);
+    public void GoToRoom7()  => LoadRoom(7);
+    public void GoToRoom8()  => LoadRoom(8);
+    public void GoToRoom9()  => LoadRoom(9);
+    public void GoToRoom10() => LoadRoom(10);
+    public void GoToRoom11() => LoadRoom(11);
+    public void GoToRoom12() => LoadRoom(12);
+    public void GoToRoom13() => LoadRoom(13);
+    public void GoToRoom14() => LoadRoom(14);
+    public void GoToRoom15() => LoadRoom(15);
+    public void GoToRoom16() => LoadRoom(16);
+    public void GoToRoom17() => LoadRoom(17);
+
+    private void LoadRoom(int roomIndex)
+    {
+        var csvPath = System.IO.Path.Combine("Content", "Dungeon", $"Room{roomIndex}.csv");
+        if (!System.IO.File.Exists(csvPath))
+        {
+            System.Console.WriteLine($"[LoadRoom] Missing CSV: {csvPath}");
+            return;
+        }
+        var csv = System.IO.File.ReadAllText(csvPath);
+        dungeon = new DungeonLoader(BlockFactory.Instance, csv);
+        dungeon.LoadRectangles();
+        link.position = new Microsoft.Xna.Framework.Vector2(100, 100);
+        collisionUpdater = new CollisionUpdater(dungeon, link);
+        System.Console.WriteLine($"[LoadRoom] Loaded Room {roomIndex}");
+    }
 
     public void ResetGame()
     {
         link = new Link(_spriteBatch);
-        
+
         blockCarousel = new BlockCarousel(blocks, _spriteBatch);
         enemyCarousel = new EnemyCarousel(enemies, _spriteBatch);
         itemCarousel = new ItemCarousel(items, _spriteBatch);
-        
+
         tile = blockCarousel.GetCurrentBlock();
         enemy = enemyCarousel.GetCurrentEnemy();
         item = itemCarousel.GetCurrentItem();
@@ -164,16 +268,51 @@ public class Game1 : Game
         controllers = new List<IController>();
         keyboard = new KeyboardController(this, null);
         controllers.Add(keyboard);
-        
+
         previousKeyboardState = Keyboard.GetState();
 
         dungeon.LoadRectangles();
-        
+
         Texture2D enemySheet = sprint0.Sprites.Texture2DStorage.GetEnemiesSpriteSheet();
-        var testEnemy = new EnemyKeese(enemySheet, new Vector2(400, 100));
+        var testEnemy = new EnemyGel(enemySheet, new Vector2(400, 100));
         dungeon.AddEnemy(testEnemy);
-        
+
         collisionUpdater = new CollisionUpdater(dungeon, link);
         collisionUpdater.getList();
+    }
+
+    private void DrawMinimapNumbers(SpriteBatch sb)
+    {
+        if (font == null) return;
+
+        int cellW = mapRect.Width / MapCols;
+        int cellH = mapRect.Height / MapRows;
+        const float scale = 0.55f;
+
+        for (int row = 0; row < MapRows; row++)
+        {
+            for (int col = 0; col < MapCols; col++)
+            {
+                int idx = row * MapCols + col;
+                if (!mapCellCommands.ContainsKey(idx))
+                    continue;
+
+                string text = $"R{idx + 1}";
+                var size = font.MeasureString(text) * scale;
+
+                var pos = new Vector2(
+                    mapRect.X + col * cellW + (cellW - size.X) * 0.5f,
+                    mapRect.Y + row * cellH + (cellH - size.Y) * 0.5f
+                );
+
+                // soft shadow
+                sb.DrawString(font, text, pos + new Vector2(1, 1),
+                              Color.Black * 0.7f, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+
+                // main label
+                sb.DrawString(font, text, pos, _minimapTextColor,
+                              0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+            }
+        }
     }
 }
