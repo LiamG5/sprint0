9cac92130e9eed294a75b5e0a858d7f93f6e800e
liamg5 <liam.e.graham@gmail.com>
2025-11-10 19:31:18 -0500

    cleaning

--- obj/Debug/net8.0/sprint0.AssemblyInfo.cs
+++ obj/Debug/net8.0/sprint0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sprint0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea3224a5b5d5928c4ef1c78d20b4e2f1dabe4d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86fe13af29d2a9001b6c7cc128c0bcb9c3c5301c")]
 [assembly: System.Reflection.AssemblyProductAttribute("sprint0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sprint0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
